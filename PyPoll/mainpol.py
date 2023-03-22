aa08c2bbd19481ac5d961bcecf735cf064d88185
Veronica Mueller <muellerveronica1@gmail.com>
2023-03-22 18:30:04 -0500

    Trying to fix percentage of votes

--- PyPoll/mainpol.py
+++ PyPoll/mainpol.py
@@ -7,8 +7,12 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 
 #create the list
 names = set()
+dict_candidates = {}
+candidate_list = []
+
 #set the starting variables to 0
 total_votes = 0
+winner = ""
 
 #Opening and reading the CSV file
 with open(csvpath) as csvfile:
@@ -20,8 +24,18 @@ with open(csvpath) as csvfile:
     for row in csvreader:
         # Keeping track of the votes
         total_votes += 1
+        candidate = row[2]
         #add the value from the candidate column to the set
         names.add(row[2])
+        if candidate not in candidate_list:
+            candidate_list.append(candidate)
+            dict_candidates[candidate] = 0
+        candidate
+for value in names:
+    percentage = round(float(names[candidate])/float(total_votes),2)
+for key in votesByCandidate.items():
+    percentage_of_votes.append(v/total_votes)
 
 print ("Total Votes: ", int(total_votes))
 print ("Candidates: ", str(names))
+print ("Percentage of Votes: ", int(percentage_of_votes))
