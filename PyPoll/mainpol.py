a63fcf919d8647ac9e6a3b68081bc707552b31ec
Veronica Mueller <muellerveronica1@gmail.com>
2023-03-21 20:54:43 -0500

    pypoll #2 updated!

--- PyPoll/mainpol.py
+++ PyPoll/mainpol.py
@@ -5,24 +5,23 @@ import csv
 #Creating an object out of the CSV file
 csvpath = os.path.join('Resources', 'election_data.csv')
 
-#create the lists
-votes = []
-
+#create the list
+names = set()
 #set the starting variables to 0
 total_votes = 0
 
 #Opening and reading the CSV file
-with open(csvpath, encoding='utf') as csvfile:
+with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile)
 
     #Reading the header row
     csv_header = next(csvreader)
 
-    total_votes += 1
-
     for row in csvreader:
         # Keeping track of the votes
-        votes.append(row[0])
         total_votes += 1
+        #add the value from the candidate column to the set
+        names.add(row[2])
 
-print (int(total_votes))
+print ("Total Votes: ", int(total_votes))
+print ("Candidates: ", str(names))
